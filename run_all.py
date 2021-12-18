26bffd031b48fcdc54e9313dd2cb0129034b8353
Traumweh <5042134-traumweh@users.noreply.gitlab.com>
2021-12-18 18:17:21 +0100

    Update: restructuring -> renamed dayxy to days/xy

--- run_all.py
+++ run_all.py
@@ -2,5 +2,5 @@
 from importlib import import_module
 
 for i in range(1, 19):
-    module = import_module(f"day{i:02}.solution")
-    print("Day{:02}:\t{}".format(i, module.Tasks(f"day{i:02}/input")))
+    module = import_module(f"days.{i:02}.solution")
+    print("Day{:02}:\t{}".format(i, module.Tasks(f"days/{i:02}/input")))
